cf500985d0085807f40534807d8d242414c85a8a
GabrielePazi <pazigabriele@gmail.com>
2023-08-28 19:15:15 +0200

    1.1 milestone 2 (se lingua "en" allora flag è USA)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script>
 import { store } from './store.js'
 import TheHeader from './components/TheHeader.vue'
+import { findFlagUrlByIso2Code } from "country-flags-svg";
+
 export default {
   components: {
     TheHeader
@@ -11,7 +13,23 @@ export default {
     }
   },
   methods: {
+    findFlagUrlByIso2Code,
+    convertEnInUs() {
+      for (let i = 0; i < store.foundMovies.length; i++) {
+        if (store.foundMovies[i].original_language == "en") {
+          store.foundMovies[i].original_language = "us"
+        }
+      }
 
+      for (let i = 0; i < store.foundSeries.length; i++) {
+        if (store.foundSeries[i].original_language == "en") {
+          store.foundSeries[i].original_language = "us"
+        }
+      }
+    }
+  },
+  updated() {
+    this.convertEnInUs()
   }
 }
 </script>
@@ -19,21 +37,40 @@ export default {
 <template>
   <div class="constainer-fluid">
     <div class="row justify-content-center">
-      <div class="col-6">
+      <div class="col-8">
         <TheHeader></TheHeader>
 
         <main>
-          <div class="d-flex gap-3 flex-wrap my-5">
-            <div class="card w-100" v-for="movie in store.foundMovies">
-            <ul class="p-3">
-              <li class="mx-3">{{ movie.title }}</li>
-              <li class="mx-3">{{ movie.original_title }}</li>
-              <li class="mx-3">{{ movie.original_language }}</li>
-              <li class="mx-3">{{ movie.vote_average }}</li>
-            </ul>
+          <div class="d-flex gap-3">
+            <div class="d-flex gap-3 flex-wrap my-5 w-50">
+              <h3>Film</h3>
+              <div class="card w-100" v-for="movie in store.foundMovies">
+                <ul class="p-3">
+                  <li class="mx-3">{{ movie.title }}</li>
+                  <li class="mx-3">{{ movie.original_title }}</li>
+                  <li class="mx-3"><img :src="findFlagUrlByIso2Code(movie.original_language)" alt=""
+                      style="width: 1.5rem;">
+                  </li>
+                  <li class="mx-3">{{ movie.vote_average }}</li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="d-flex gap-3 flex-wrap my-5 w-50">
+              <h3>Serie</h3>
+              <div class="card w-100" v-for="serie in store.foundSeries">
+                <ul class="p-3">
+                  <li class="mx-3">{{ serie.name }}</li>
+                  <li class="mx-3">{{ serie.original_name }}</li>
+                  <li class="mx-3"><img :src="findFlagUrlByIso2Code(serie.original_language)" alt=""
+                      style="width: 1.5rem;">
+                  </li>
+                  <li class="mx-3">{{ serie.vote_average }}</li>
+                </ul>
+              </div>
+            </div>
           </div>
-          </div>
-          
+
         </main>
       </div>
     </div>
--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
-import axios from  'axios'
+import axios from 'axios'
 import { reactive } from 'vue'
 
 export const store = reactive({
   apiKey: "a0ff6243024fc680740f6a0afe993562",
   movieToSearch: "",
-  foundMovies: []
+  foundMovies: [],
+  foundSeries: []
 })
 
 export function searchMovie() {
@@ -14,5 +15,17 @@ export function searchMovie() {
   axios.get(url).then((response) => {
     store.foundMovies.push(...response.data.results)
     console.log(store.foundMovies)
+  })
+
+  searchSeries()
+}
+
+export function searchSeries() {
+  store.foundSeries = []
+  const url = `https://api.themoviedb.org/3/search/tv?api_key=${store.apiKey}&query=${store.movieToSearch}&language=it-IT`
+
+  axios.get(url).then((response) => {
+    store.foundSeries.push(...response.data.results)
+    console.log(store.foundSeries)
   })
 }
